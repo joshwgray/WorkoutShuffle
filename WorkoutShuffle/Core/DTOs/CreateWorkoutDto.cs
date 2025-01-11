857f021e89c18f0790857c1e230e7d8741ebcc30
Josh Gray <josh.w.gray@gmail.com>
2025-01-11 11:40:07 +0000

    Use type keyword for string values

--- WorkoutShuffle/Core/DTOs/CreateWorkoutDto.cs
+++ WorkoutShuffle/Core/DTOs/CreateWorkoutDto.cs
@@ -3,21 +3,21 @@ namespace WorkoutShuffle.Core.DTOs;
 public class CreateWorkoutDto                                  
 {                                                              
     public Guid Id { get; set; }                               
-    public string CourseHeader { get; set; } = String.Empty;   
-    public string Version { get; set; } = String.Empty;        
-    public string Units { get; set; } = String.Empty;          
-    public string Description { get; set; } = String.Empty;    
-    public string FileName { get; set; } = String.Empty;       
-    public string Ftp { get; set; } = String.Empty;            
-    public string MinutesWatts { get; set; } = String.Empty;   
-    public string EndCourseHeader { get; set; } = String.Empty;
+    public string CourseHeader { get; set; } = string.Empty;   
+    public string Version { get; set; } = string.Empty;        
+    public string Units { get; set; } = string.Empty;          
+    public string Description { get; set; } = string.Empty;    
+    public string FileName { get; set; } = string.Empty;       
+    public string Ftp { get; set; } = string.Empty;            
+    public string MinutesWatts { get; set; } = string.Empty;   
+    public string EndCourseHeader { get; set; } = string.Empty;
     public List<WorkoutStep> WorkoutSteps { get; set; }        
-    public string CourseData { get; internal set; }            
-    public string EndCourseData { get; internal set; }         
+    public string? CourseData { get; internal set; }            
+    public string? EndCourseData { get; internal set; }         
 }                                                              
                                                                
 public class WorkoutStep                                       
 {                                                              
-    public string Time { get; set; }                           
-    public string Power { get; set; }                          
+    public string? Time { get; set; }                           
+    public string? Power { get; set; }                          
 }                                                              
